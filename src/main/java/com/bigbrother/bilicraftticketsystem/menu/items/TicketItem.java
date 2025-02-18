5e9e8fe2dfca32edc179410a9bd700f21c72e7a5
dyh <2361624529@qq.com>
2025-02-18 17:49:56 +0800

    feat: 创造模式可使用中键取出车票

--- src/main/java/com/bigbrother/bilicraftticketsystem/menu/items/TicketItem.java
+++ src/main/java/com/bigbrother/bilicraftticketsystem/menu/items/TicketItem.java
@@ -50,27 +50,29 @@ public class TicketItem extends AbstractItem {
             return;
         }
 
-        EconomyResponse r = econ.withdrawPlayer(player, ticket.getTotalPrice());
+        if (!clickType.isCreativeAction()) {
+            EconomyResponse r = econ.withdrawPlayer(player, ticket.getTotalPrice());
+            if (r.transactionSuccess()) {
+                ticket.give();
 
-        if (r.transactionSuccess()) {
-            ticket.give();
-
-            player.sendMessage(
-                    MiniMessage.miniMessage().deserialize(message.get("buy-success", "您成功花费 %.2f 购买了 %s")
-                            .formatted(r.amount, ticket.getItemName())).decoration(TextDecoration.ITALIC, false));
-            // 记录log
-            Bukkit.getConsoleSender().sendMessage(MiniMessage.miniMessage().deserialize(
-                    "<gold>[帕拉伦国有铁路车票系统] <green>玩家 %s 成功花费 %.2f 购买了 %s"
-                            .formatted(player.getName(), r.amount, ticket.getItemName())));
-            // 记录到数据库
-            trainDatabaseManager.addTicketInfo(player.getName(), player.getUniqueId().toString(), r.amount, CommonItemStack.of(ticket.getTicket()).getCustomData());
+                player.sendMessage(
+                        MiniMessage.miniMessage().deserialize(message.get("buy-success", "您成功花费 %.2f 购买了 %s")
+                                .formatted(r.amount, ticket.getItemName())).decoration(TextDecoration.ITALIC, false));
+                // 记录log
+                Bukkit.getConsoleSender().sendMessage(MiniMessage.miniMessage().deserialize(
+                        "<gold>[帕拉伦国有铁路车票系统] <green>玩家 %s 成功花费 %.2f 购买了 %s"
+                                .formatted(player.getName(), r.amount, ticket.getItemName())));
+                // 记录到数据库
+                trainDatabaseManager.addTicketInfo(player.getName(), player.getUniqueId().toString(), r.amount, CommonItemStack.of(ticket.getTicket()).getCustomData());
+            } else {
+                player.sendMessage(MiniMessage.miniMessage().deserialize(
+                                message.get("buy-failure", "车票购买失败：%s")
+                                        .formatted(r.errorMessage))
+                        .decoration(TextDecoration.ITALIC, false));
+            }
         } else {
-            player.sendMessage(MiniMessage.miniMessage().deserialize(
-                            message.get("buy-failure", "车票购买失败：%s")
-                                    .formatted(r.errorMessage))
-                    .decoration(TextDecoration.ITALIC, false));
+            ticket.give();
         }
-
         MenuMain.getMenu(player).close();
     }
 
