03870c6fc46527626b8ef5ff677395f82827ea30
dyh <2361624529@qq.com>
2025-03-13 16:25:35 +0800

    perf: 删除返回按钮的lore

--- src/main/java/com/bigbrother/bilicraftticketsystem/menu/items/FilterBackItem.java
+++ src/main/java/com/bigbrother/bilicraftticketsystem/menu/items/FilterBackItem.java
@@ -1,11 +1,7 @@
 package com.bigbrother.bilicraftticketsystem.menu.items;
 
 import com.bigbrother.bilicraftticketsystem.Utils;
-import com.bigbrother.bilicraftticketsystem.menu.MenuFilter;
 import com.bigbrother.bilicraftticketsystem.menu.MenuMain;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -15,20 +11,10 @@ import xyz.xenondevs.invui.item.ItemProvider;
 import xyz.xenondevs.invui.item.builder.ItemBuilder;
 import xyz.xenondevs.invui.item.impl.AbstractItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FilterBackItem extends AbstractItem {
     @Override
     public ItemProvider getItemProvider(Player player) {
         ItemStack itemStack = Utils.loadItemFromFile("back");
-        MenuFilter menuFilter = MenuFilter.getMenu(player);
-        List<Component> lore = itemStack.lore();
-        if (lore == null) {
-            lore = new ArrayList<>();
-        }
-        lore.add(Component.text("已选择的途径车站：", NamedTextColor.GOLD).decoration(TextDecoration.ITALIC, false));
-        lore.add(FilterItem.getFormattedComponent(menuFilter).decoration(TextDecoration.ITALIC, false));
         return new ItemBuilder(itemStack);
     }
 
